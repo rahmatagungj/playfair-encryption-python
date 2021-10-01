fadc8db99141971d13ca8bc655492f6034c79493
Rahmat Agung Julians <rahmatagungj@Gmail.com>
2021-10-01 18:16:28 +0700

    first init
    init project with main function

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    key = input("Enter a key: ").replace(" ", "").upper()
+    plaintext = input("Enter a plaintext: ").replace(" ", "").upper()
+
+if __name__ == "__main__":
+    main()
